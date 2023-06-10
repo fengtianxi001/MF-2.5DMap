4617ea84dae9f78802a37b5f5103d8af5bc8c5eb
肛肠科冯主任 <625362207@qq.com>
2023-06-10 19:56:56 +0800

    feat: 清空分支

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import MyMap from "./scripts/MyMap";
-import './styles/index.scss';
-const app = document.getElementById("app") as HTMLElement;
-const myMap = new MyMap(app);
-myMap.loadData("https://geojson.cn/data/100000.json");
